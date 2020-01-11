5247d8460085e390736a383670db625103cc02a2
moonti <tmmoond8@gmail.com>
2020-01-11 13:25:23 +0900

    add classic ckeditor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import CKEditor from './CKEditor';
 
 function App() {
+  const [ contents, setContents ] = useState('');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CKEditor onChange={setContents}/>
+      <pre style={{ whiteSpace: 'normal'}}>
+        {contents}
+      </pre>
     </div>
   );
 }
---
+++ src/CKEditor.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import CKEditor5 from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './CKEditor.css';
+
+const CKEditor = (props) => {
+  const { content, onChange } = props;
+  return (
+    <div id="TammCKEditor">
+      <CKEditor5
+        editor={ ClassicEditor }
+        data={content}
+        onInit={ editor => {
+            // You can store the "editor" and use when it is needed.
+            // console.log( 'Editor is ready to use!', editor );
+        } }
+        onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            onChange(data);
+            // console.log( { event, editor, data } );
+        } }
+        onBlur={ ( event, editor ) => {
+          const data = editor.getData();
+            console.log(data );
+        } }
+        onFocus={ ( event, editor ) => {
+            // console.log( 'Focus.', editor );
+        } }
+      />
+    </div>
+  );
+}
+
+export default CKEditor;
