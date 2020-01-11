b5a4f88ad827aeae896fa77a9618883cc2617d8d
moonti <tmmoond8@gmail.com>
2020-01-11 16:34:41 +0900

    setup custom ckeditor5

--- src/CKEditor5.js
+++ src/CKEditor5.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import CKEditor5 from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+// NOTE: We use editor from source (not a build)!
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import './CKEditor5.css';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+const editorConfiguration = {
+  plugins: [ Essentials, Bold, Italic, Paragraph ],
+  toolbar: [ 'bold', 'italic' ]
+};
 
 const CKEditor = (props) => {
   const { content, onChange } = props;
   return (
     <div id="TammCKEditor">
       <CKEditor5
-        editor={ ClassicEditor }
-        data={content}
-        onInit={ editor => {
+          editor={ ClassicEditor }
+          config={ editorConfiguration }
+          data={content}
+          onInit={ editor => {
             // You can store the "editor" and use when it is needed.
-            // console.log( 'Editor is ready to use!', editor );
+            console.log( 'Editor is ready to use!', editor );
+          } }
+          onChange={ ( event, editor ) => {
+            onChange(editor.getData());
         } }
-        onChange={ ( event, editor ) => {
-            const data = editor.getData();
-            onChange(data);
-            // console.log( { event, editor, data } );
-        } }
-        onBlur={ ( event, editor ) => {
-          const data = editor.getData();
-            console.log(data );
-        } }
-        onFocus={ ( event, editor ) => {
-            // console.log( 'Focus.', editor );
-        } }
-      />
+          onBlur={ editor => {
+            console.log( 'Blur.', editor );
+          } }
+          onFocus={ editor => {
+            console.log( 'Focus.', editor );
+          } }
+        />
     </div>
   );
 }
